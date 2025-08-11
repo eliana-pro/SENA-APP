07d1e447a7c2531bc325f685c9b4f073da0529a6
Eliana <1206velandia.g@gmail.com>
2025-08-11 12:28:30 -0500

    4 y 5 Parte del trabajo

--- SENA_APP/aprendices/views.py
+++ SENA_APP/aprendices/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Aprendiz
+from .models import Curso
+from django.shortcuts import get_object_or_404
 def aprendices(request):
     lista_aprendices = Aprendiz.objects.all().order_by('apellido', 'nombre')
     template = loader.get_template('lista_aprendices.html')
@@ -12,4 +14,47 @@ def aprendices(request):
 
 def inicio(request):
     template = loader.get_template('inicio.html')
+    total_aprendices = Aprendiz.objects.count()
+    total_cursos = Curso.objects.count()
+    cursos_activos = Curso.objects.filter(estado__in=['INI', 'EJE']).count()
+    
+    context = {
+        'total_aprendices': total_aprendices,
+        'total_cursos': total_cursos,
+        'cursos_activos': cursos_activos,
+    }
     return HttpResponse(template.render())
+
+
+def lista_cursos(request):
+    cursos = Curso.objects.all().order_by('-fecha_inicio')
+    template = loader.get_template('lista_cursos.html')
+    context = {
+        'lista_cursos': cursos,
+        'total_cursos': cursos.count(),
+        'titulo': 'Lista de Cursos',
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detalle_curso(request, curso_id):
+    curso = get_object_or_404(Curso, id=curso_id)
+    aprendices_curso = curso.aprendizcurso_set.all()
+    instructores_curso = curso.instructorcurso_set.all()
+    template = loader.get_template('detalle_curso.html')
+    
+    context = {
+        'curso': curso,
+        'aprendices_curso': aprendices_curso,
+        'instructores_curso': instructores_curso,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detalle_aprendiz(request, aprendiz_id):
+    aprendiz = get_object_or_404(Aprendiz, id=aprendiz_id)
+    template = loader.get_template('detalle_aprendiz.html')
+    
+    context = {
+        'aprendiz': aprendiz,
+    }
+    return HttpResponse(template.render(context, request))
--- SENA_APP/instructores/views.py
+++ SENA_APP/instructores/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Instructor
-#Create your views here.
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
 
 def instructores(request):
     lista_instructores = Instructor.objects.all().order_by('apellido', 'nombre')
     template = loader.get_template('lista_instructores.html')
     context = {
-        'lista_instructores': lista_instructores,
-        'total_instructores': lista_instructores.count(),
+    'lista_instructores': lista_instructores,
+    'total_instructores': lista_instructores.count(),
     }
+    
+    return HttpResponse(template.render(context, request))
+
+def detalle_instructor(request, instructor_id):
+    instructor = get_object_or_404(Instructor, id=instructor_id)
+    cursos_coordinados = instructor.cursos_coordinados.all()
+    cursos_impartidos = instructor.cursos_impartidos.all()
+    template = loader.get_template('detalle_instructor.html')
+    
+    context = {
+        'instructor': instructor,
+        'cursos_coordinados': cursos_coordinados,
+        'cursos_impartidos': cursos_impartidos,
+    }
+    
     return HttpResponse(template.render(context, request))
